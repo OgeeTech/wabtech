4f5267b8ac3e4385dd42757620eda04010a88206
OgeeTech <wgift41@gmail.com>
2026-08-23 07:38:43 +0100

    refectored app

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,10 @@
+<!-- src/App.vue -->
 <script setup lang="ts">
-import HeroSection from "@/components/HeroSection.vue";
-import NavBar from "./components/NavBar.vue";
-import About from "./components/About.vue";
-import ServicesSection from "./components/ServicesSection.vue";
-import ContactSection from "./components/ContactSection.vue";
-import Footer from "./components/Footer.vue";
+import { RouterView } from "vue-router";
 </script>
 
 <template>
-  <main>
-    <NavBar />
-    <HeroSection />
-    <About />
-    <ServicesSection />
-    <ContactSection />
-    <Footer />
-  </main>
+  <RouterView />
 </template>
 
 <style scoped></style>
--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,8 +1,20 @@
-<script setup lang="ts"></script>
+<!-- src/views/Index.vue -->
+<script setup lang="ts">
+import NavBar from "@/components/NavBar.vue";
+import HeroSection from "@/components/HeroSection.vue";
+import About from "@/components/About.vue";
+import ServicesSection from "@/components/ServicesSection.vue";
+import ContactSection from "@/components/ContactSection.vue";
+import Footer from "@/components/Footer.vue";
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>welcome to your new Vue.js app</p>
+  <main>
+    <NavBar />
+    <HeroSection />
+    <About />
+    <ServicesSection />
+    <ContactSection />
+    <Footer />
+  </main>
 </template>
-
-<style scoped></style>
